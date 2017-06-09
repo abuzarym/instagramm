9901731ab3c3fc5a846fbc3e8686f29e219b25da
Abu Zarym Sulaiman <Zarym1990@gmail.com>
2017-06-09 11:23:41 +0800

    Paperclip gem and uploading pictures

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -12,8 +12,7 @@ class PicsController < ApplicationController
 		@pic = current_user.pics.build
 	end
 
-	def create
-			
+	def create	
 		@pic = current_user.pics.build(pic_params)
 
 		if @pic.save 
@@ -42,7 +41,7 @@ class PicsController < ApplicationController
 	private
 
 	def pic_params
-		params.require(:pic).permit(:title , :description)
+		params.require(:pic).permit(:title , :description, :image)
 	end
 
 	def find_pic
